4fdcd7d59855b1ba235ea7435f1701103d0e4bea
Mayank Jain <jainmayank16041999@gmail.com>
2021-07-04 17:56:36 +0530

    home routes added

--- projbackend/controllers/homeScreen.js
+++ projbackend/controllers/homeScreen.js
@@ -1,4 +1,5 @@
 const ProductSchema = require("../models/product");
+const CategorySchema = require("../models/category");
 
 exports.getRandomProducts = async (req, res) => {
 	ProductSchema.aggregate([{ $sample: { size: req.body.count } }]).exec(
@@ -39,4 +40,46 @@ exports.getTopRatedProductsBasedOnCategoryId = async (req, res) => {
 			}
 			return res.status(200).json(products);
 		});
+};
+
+exports.getRandomCategory = async (req, res) => {
+	CategorySchema.aggregate([{ $sample: { size: 2 } }]).exec(
+		(err, categorys) => {
+			if (err || !categorys) {
+				return res.status(400).json({
+					error: "Category not found in DB",
+				});
+			}
+			return res.status(200).json(categorys);
+		}
+	);
+};
+
+exports.getNewlyArrivedProduct = async (req, res) => {
+	ProductSchema.find()
+		.sort({ createdAt: "descending" })
+		.limit(6)
+		.exec((err, products) => {
+			if (err || !products) {
+				return res.status(400).json({
+					error: "Products not found in DB",
+				});
+			}
+			return res.status(200).json(products);
+		});
+};
+
+exports.getProductBasedOnTribe = async (req, res) => {
+	if (req.body.tribe == null || req.body.tribe.length == 0) {
+		return res.status(400).json({ error: "tribe not mentioned" });
+	}
+
+	ProductSchema.find({ tribe: req.body.tribe }).exec((err, products) => {
+		if (err || !products) {
+			return res.status(400).json({
+				error: "Products not found in DB",
+			});
+		}
+		return res.status(200).json(products);
+	});
 };
